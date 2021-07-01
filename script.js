e789f84ac7ede7ae8c7ffeca86373c71f689be5b
Mark Kavanagh <markkavanagh91@gmail.com>
2021-07-01 12:26:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+async function getPokemon(name){
+    var response = await fetch("https://pokeapi.co/api/v2/pokemon/" + name);
+    var data = await response.json();
+    //console.log(data.sprites.front_default);
+
+    var image = document.getElementById("pkmn");
+    image.src = data.sprites.front_default;
+    image.alt = name;
+}
+
+getPokemon("pikachu");
+
+function searchForPokemon(){
+    var pkmnInput = document.getElementById("pkmnInput");
+    var name = pkmnInput.value;
+
+    getPokemon(name);
+}
